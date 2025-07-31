b64be81dadfc560a2a4f33dcc4c2d79a8544e951
KingClarke <D00267783@student.dkit.ie>
2025-08-01 00:21:47 +0100

    Part A - Needs some quick fixes

--- Customer.cpp
+++ Customer.cpp
@@ -98,7 +98,7 @@ public:
         return !(*this == other);
     }
 
-    // Overloaded stream insertion and extraction operators // ### check
+    // Overloaded stream insertion and extraction operators // ### check, copied form older project dont really undertant ###REMEMBER TO CHECK###
     friend ostream& operator<<(ostream& os, const Customer& c) {
         os << c.customerId << " " << c.title << " " << c.name << " " << c.numberOfPurchases << " " << c.type;
         for (int i = 0; i < c.numberOfPurchases; ++i) {
